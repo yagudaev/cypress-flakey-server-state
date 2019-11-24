3209980e07f2cf0cf8eff56c24ff5b715797c267
Michael Yagudaev <michaelyagudaev@gmail.com>
2019-11-23 23:28:42 -0800

    setup basic incrementer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    setCount(count + 1)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Counter
       </header>
+      <p>{count}</p>
+      <button onClick={increment}>Increment</button>
     </div>
   )
 }
